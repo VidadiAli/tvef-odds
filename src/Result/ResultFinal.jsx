ae09de8efead1eb740883d9ad3877b077f45d722
VidadiAli <vidadiali.tiva@gmail.com>
2024-07-18 02:03:02 +0400

    first commit

--- src/Result/ResultFinal.jsx
+++ src/Result/ResultFinal.jsx
@@ -6,160 +6,160 @@ import Result from './Result';
 import { LuMoveDown } from "react-icons/lu";
 
 const ResultFinal = () => {
-    // const [mainArray, setMainArray] = useState([]);
-    // const [mainArrayTele, setMainArrayTele] = useState([]);
-    // const [changedArray, setChangedArray] = useState([]);
-    // const [secondArray, setSecondArray] = useState([]);
-    // const [countryName, setCountryName] = useState([]);
-    // const [points, setPoints] = useState([]);
-    // const [finalCountry, setfinalCountry] = useState([]);
-    // const [changeNumber, setChangeNumber] = useState(0);
-    // const [changeNumber2, setChangeNumber2] = useState(0);
-    // const [waitClass, setWaitClass] = useState('');
-    // const [itemPuanTele, setItemPuanTele] = useState('')
-    // const [b, setb] = useState([]);
+    const [mainArray, setMainArray] = useState([]);
+    const [mainArrayTele, setMainArrayTele] = useState([]);
+    const [changedArray, setChangedArray] = useState([]);
+    const [secondArray, setSecondArray] = useState([]);
+    const [countryName, setCountryName] = useState([]);
+    const [points, setPoints] = useState([]);
+    const [finalCountry, setfinalCountry] = useState([]);
+    const [changeNumber, setChangeNumber] = useState(0);
+    const [changeNumber2, setChangeNumber2] = useState(0);
+    const [waitClass, setWaitClass] = useState('');
+    const [itemPuanTele, setItemPuanTele] = useState('')
+    const [b, setb] = useState([]);
 
-    // const callData = async () => {
-    //     setWaitClass('wait-vote-adding');
+    const callData = async () => {
+        setWaitClass('wait-vote-adding');
 
-    //     const data = (await axios.get(`https://us-central1-api-tvef-vote.cloudfunctions.net/app/readPuans`)).data;
-    //     setMainArray(data);
+        const data = (await axios.get(`https://us-central1-api-tvef-vote.cloudfunctions.net/app/readPuans`)).data;
+        setMainArray(data);
 
-    //     const data1 = (await axios.get(`https://us-central1-api-tvef-vote.cloudfunctions.net/app/readTelePuans`)).data;
-    //     setMainArrayTele(data1);
+        const data1 = (await axios.get(`https://us-central1-api-tvef-vote.cloudfunctions.net/app/readTelePuans`)).data;
+        setMainArrayTele(data1);
 
-    //     const updatedCountryNames = Array.from(new Set(data.map((element) => element.givinCountry)));
+        const updatedCountryNames = Array.from(new Set(data.map((element) => element.givinCountry)));
 
-    //     const finalData = (await axios.get(`${mainUrl}edition9`)).data;
-    //     setSecondArray(finalData.final);
+        const finalData = (await axios.get(`${mainUrl}edition9`)).data;
+        setSecondArray(finalData.final);
 
-    //     const updatedfinalCountry = Array.from(new Set(finalData.final.map((element) => element.countryName)));
+        const updatedfinalCountry = Array.from(new Set(finalData.final.map((element) => element.countryName)));
 
-    //     setfinalCountry(updatedfinalCountry);
-    //     setCountryName(updatedCountryNames);
+        setfinalCountry(updatedfinalCountry);
+        setCountryName(updatedCountryNames);
 
-    //     createPoints(data, data1, updatedCountryNames);
+        createPoints(data, data1, updatedCountryNames);
 
-    //     setChangedArray(data);
-    //     setWaitClass('');
-    // };
+        setChangedArray(data);
+        setWaitClass('');
+    };
 
-    // const order = (countryName, points) => {
-    //     const sorted = countryName.map((cn, index) => ({ cn, pt: points[index] }))
-    //         .sort((a, b) => b.pt - a.pt);
+    const order = (countryName, points) => {
+        const sorted = countryName.map((cn, index) => ({ cn, pt: points[index] }))
+            .sort((a, b) => b.pt - a.pt);
 
-    //     setCountryName(sorted.map(item => item.cn));
-    //     setPoints(sorted.map(item => item.pt));
-    // };
+        setCountryName(sorted.map(item => item.cn));
+        setPoints(sorted.map(item => item.pt));
+    };
 
-    // const createPoints = (data, data1, updatedCountryNames) => {
-    //     const updatedPoints = updatedCountryNames.map((e) => {
-    //         const totalPoints = [];
-    //         data1.forEach((element) => {
-    //             totalPoints.push(element);
-    //         });
-    //         data.forEach((element) => {
-    //             totalPoints.push(element);
-    //         });
-    //         return totalPoints.filter(j => e === j.getingCountry).reduce((sum, j) => sum + j.puan, 0);
-    //     });
+    const createPoints = (data, data1, updatedCountryNames) => {
+        const updatedPoints = updatedCountryNames.map((e) => {
+            const totalPoints = [];
+            data1.forEach((element) => {
+                totalPoints.push(element);
+            });
+            data.forEach((element) => {
+                totalPoints.push(element);
+            });
+            return totalPoints.filter(j => e === j.getingCountry).reduce((sum, j) => sum + j.puan, 0);
+        });
 
-    //     setPoints(updatedPoints);
-    //     order(updatedCountryNames, updatedPoints);
-    // };
+        setPoints(updatedPoints);
+        order(updatedCountryNames, updatedPoints);
+    };
 
-    // const changeElement = (e) => {
-    //     const selectedCountry = e.getAttribute('alt') || e.textContent;
+    const changeElement = (e) => {
+        const selectedCountry = e.getAttribute('alt') || e.textContent;
 
-    //     if (countryName.includes(selectedCountry)) {
-    //         localStorage.setItem('ed-9-result', selectedCountry);
-    //         localStorage.setItem('choosenCountryForResult', selectedCountry);
-    //         setChangeNumber(prev => prev + 1);
+        if (countryName.includes(selectedCountry)) {
+            localStorage.setItem('ed-9-result', selectedCountry);
+            localStorage.setItem('choosenCountryForResult', selectedCountry);
+            setChangeNumber(prev => prev + 1);
 
-    //         const filtered = changedArray.filter(j => j.getingCountry === selectedCountry)
-    //             .map(j => ({
-    //                 ...secondArray.find(f => j.givinCountry === f.countryName),
-    //                 puan: j.puan
-    //             }));
+            const filtered = changedArray.filter(j => j.getingCountry === selectedCountry)
+                .map(j => ({
+                    ...secondArray.find(f => j.givinCountry === f.countryName),
+                    puan: j.puan
+                }));
 
-    //         setb(filtered.sort((a, b) => a.puan - b.puan));
-    //         setChangeNumber2(1);
-    //     }
-    // };
+            setb(filtered.sort((a, b) => a.puan - b.puan));
+            setChangeNumber2(1);
+        }
+    };
 
-    // const showCountryPoints = (e) => {
-    //     const selectedCountry = localStorage.getItem('choosenCountryForResult');
+    const showCountryPoints = (e) => {
+        const selectedCountry = localStorage.getItem('choosenCountryForResult');
 
-    //     const filtered = changedArray.filter(j => j.givinCountry === selectedCountry)
-    //         .map(j => ({
-    //             ...secondArray.find(f => j.getingCountry === f.countryName),
-    //             puan: j.puan
-    //         }));
+        const filtered = changedArray.filter(j => j.givinCountry === selectedCountry)
+            .map(j => ({
+                ...secondArray.find(f => j.getingCountry === f.countryName),
+                puan: j.puan
+            }));
 
-    //     setb(filtered.sort((a, b) => a.puan - b.puan));
-    //     setChangeNumber2(0);
-    //     setChangeNumber(0);
-    // };
+        setb(filtered.sort((a, b) => a.puan - b.puan));
+        setChangeNumber2(0);
+        setChangeNumber(0);
+    };
 
-    // useEffect(() => {
-    //     callData();
-    //     const showAllPuansElement = document.getElementsByClassName('show-all-puans')[0];
-    //     showAllPuansElement.style.height = showAllPuansElement.offsetWidth + 80 + 'px';
-    // }, []);
+    useEffect(() => {
+        callData();
+        const showAllPuansElement = document.getElementsByClassName('show-all-puans')[0];
+        showAllPuansElement.style.height = showAllPuansElement.offsetWidth + 80 + 'px';
+    }, []);
 
-    // useEffect(() => {
-    //     if (countryName.length > 0) {
-    //         const defaultCountry = countryName[0];
-    //         localStorage.setItem('choosenCountryForResult', defaultCountry);
+    useEffect(() => {
+        if (countryName.length > 0) {
+            const defaultCountry = countryName[0];
+            localStorage.setItem('choosenCountryForResult', defaultCountry);
 
-    //         const filtered = changedArray.filter(j => j.givinCountry === defaultCountry)
-    //             .map(j => ({
-    //                 ...secondArray.find(f => j.getingCountry === f.countryName),
-    //                 puan: j.puan
-    //             }));
+            const filtered = changedArray.filter(j => j.givinCountry === defaultCountry)
+                .map(j => ({
+                    ...secondArray.find(f => j.getingCountry === f.countryName),
+                    puan: j.puan
+                }));
 
-    //         setb(filtered.sort((a, b) => a.puan - b.puan));
-    //         setChangeNumber2(0);
-    //         setChangeNumber(0);
-    //     }
-    // }, [countryName]);
+            setb(filtered.sort((a, b) => a.puan - b.puan));
+            setChangeNumber2(0);
+            setChangeNumber(0);
+        }
+    }, [countryName]);
 
-    // useEffect(() => {
-    //     const selectedCountry = localStorage.getItem('choosenCountryForResult');
-    //     const filtered = changedArray.filter(j => j.givinCountry === selectedCountry)
-    //         .map(j => ({
-    //             ...secondArray.find(f => j.getingCountry === f.countryName),
-    //             puan: j.puan
-    //         }));
-    //     setb(filtered.sort((a, b) => a.puan - b.puan));
-    // }, [changedArray]);
+    useEffect(() => {
+        const selectedCountry = localStorage.getItem('choosenCountryForResult');
+        const filtered = changedArray.filter(j => j.givinCountry === selectedCountry)
+            .map(j => ({
+                ...secondArray.find(f => j.getingCountry === f.countryName),
+                puan: j.puan
+            }));
+        setb(filtered.sort((a, b) => a.puan - b.puan));
+    }, [changedArray]);
 
-    // const showJury = () => {
-    //     setChangedArray(mainArray);
-    //     setItemPuanTele('');
-    //     setChangeNumber2(0);
-    // };
+    const showJury = () => {
+        setChangedArray(mainArray);
+        setItemPuanTele('');
+        setChangeNumber2(0);
+    };
 
-    // const showTele = () => {
-    //     setChangedArray(mainArrayTele);
-    //     setItemPuanTele('item-puan-tele');
-    //     setChangeNumber2(0);
-    // };
+    const showTele = () => {
+        setChangedArray(mainArrayTele);
+        setItemPuanTele('item-puan-tele');
+        setChangeNumber2(0);
+    };
 
-    // const renderCountryPoints = (startIndex, endIndex) => {
-    //     return b.slice(startIndex, endIndex).map((item, index) => {
-    //         return (
-    //             <div key={index} className={`${itemPuanTele}`} style={{ width: '35px', height: '35px' }}>
-    //                 <img src={item.flag} alt={item.countryName} />
-    //                 <span style={{ width: '18px', height: '18px', fontSize: '.7rem' }}>{item.puan}</span>
-    //             </div>
-    //         );
-    //     });
-    // };
+    const renderCountryPoints = (startIndex, endIndex) => {
+        return b.slice(startIndex, endIndex).map((item, index) => {
+            return (
+                <div key={index} className={`${itemPuanTele}`} style={{ width: '35px', height: '35px' }}>
+                    <img src={item.flag} alt={item.countryName} />
+                    <span style={{ width: '18px', height: '18px', fontSize: '.7rem' }}>{item.puan}</span>
+                </div>
+            );
+        });
+    };
 
     return (
         <>
-            {/* <div className='result'>
+         <div className='result'>
             <div className={`wait-vote ${waitClass}`}>
                 <button>please wait ...</button>
             </div>
@@ -223,9 +223,8 @@ const ResultFinal = () => {
                     });
                 })}
             </div>
-        </div> */}
+        </div> 
 
-            <div>Üzərində işlənir...</div>
         </>
     );
 };
