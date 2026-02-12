45da2de83c8bd999bf5138b5047198c39542cfa7
VidadiAli <vidadiali.tiva@gmail.com>
2026-02-13 00:36:53 +0400

    last changed

--- src/More/More.jsx
+++ src/More/More.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import './More.css'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import './More.css';
 
 const More = () => {
-    return (
-        <div className='more-element'>
-            <NavLink to={'/tvef-odds/edition-9-final'}>The Voice Of EuroFans - Edition 9</NavLink>
-            <NavLink to={'/tvef-odds/edition-8-final'}>The Voice Of EuroFans - Edition 8</NavLink>
-            <NavLink to={'/tvef-odds/edition-7-final'}>The Voice Of EuroFans - Edition 7</NavLink>
-        </div>
-    )
+  return (
+    <div className='more-element'>
+      <NavLink 
+        to='/tvef-odds/edition-9-final' 
+        className={({ isActive }) => isActive ? 'active-link' : ''}
+      >
+        The Voice Of EuroFans - Edition 9
+      </NavLink>
+      <NavLink 
+        to='/tvef-odds/edition-8-final' 
+        className={({ isActive }) => isActive ? 'active-link' : ''}
+      >
+        The Voice Of EuroFans - Edition 8
+      </NavLink>
+      <NavLink 
+        to='/tvef-odds/edition-7-final' 
+        className={({ isActive }) => isActive ? 'active-link' : ''}
+      >
+        The Voice Of EuroFans - Edition 7
+      </NavLink>
+    </div>
+  )
 }
 
-export default More
+export default More;
